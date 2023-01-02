13cb6ba0af3ba174370eeeca67f18dbfc4e4539f
JeewanAidi <aidizeon@gmail.com>
2023-01-01 20:16:52 -0500

    Unit converter (kg to pounds)

---
+++ app/src/main/java/com/example/unitconvo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.unitconvo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205 * kg;
+                textView.setText(kg + " kg = " + pound + "pounds");
+
+            }
+        });
+
+
+    }
+}
